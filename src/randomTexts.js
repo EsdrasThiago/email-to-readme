76c848844e3197fccd79536423a7b875e74a5aeb
Esdras Thiago <esdras12thiago@gmail.com>
2023-02-17 04:06:36 -0300

    Adicionando texto
    Adicionando um texto com um calculo de probabilidade

--- src/randomTexts.js
+++ src/randomTexts.js
@@ -1,13 +1,24 @@
+
+
 const randomText = [
-'Meu email',
+'Meu email!',
 'A probabilidade de cair as três cores perfeitas é de 0,0019%',
 'Oque será que acontece se cair três cores perfeitas?...',
-'Opa moshi moshi, boa tarde',
+'Opa moshi moshi, boa tarde!',
 ]
+
+const chanceText = () => {
+  const totalTexts = randomText.length();
+  const totalCalc = 1 / totalTexts;
+  const calcPercent = totalCalc * 100;
+  const text = `A chance dessa mensagem aparecer é de ${calcPercent}%`,
+  randomText.push(text);
+}
 
 export const randomTextSelector = () => {
   const newMin = Math.ceil(1);
   const newMax = Math.floor(randomText.length);
+  chanceText();
   const randomPosition = Math.floor(Math.random() * (newMax - newMin)) + newMin;
-  return randomText[randomPosition]
+  return randomText[randomPosition];
 }
