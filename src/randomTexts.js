893b075edaac9ac7277a0ee53b65350d73fdf1c5
Esdras Thiago <esdras12thiago@gmail.com>
2023-02-17 03:50:22 -0300

    Create randomTexts.js
    adicionando textos e função para gerar eles aleatoriamente

---
+++ src/randomTexts.js
@@ -0,0 +1,15 @@
+const randomText = [
+'Meu email',
+'A probabilidade de cair as três cores perfeitas é de 0,0019%',
+'Oque será que acontece se cair três cores perfeitas?...',
+'Opa moshi moshi, boa tarde',
+]
+
+const randomTextSelector = () => {
+  const newMin = Math.ceil(1);
+  const newMax = Math.floor(randomText.length);
+  const randomPosition = Math.floor(Math.random() * (newMax - newMin)) + newMin;
+  return randomText[randomPosition]
+}
+
+export default randomTextSelector
