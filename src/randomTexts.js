0ceec4d68dc9ef8fee738ba99021aa8940bef811
Esdras Thiago <esdras12thiago@gmail.com>
2023-02-17 04:09:55 -0300

    Comentando alterações

--- src/randomTexts.js
+++ src/randomTexts.js
@@ -7,18 +7,18 @@ const randomText = [
 'Opa moshi moshi, boa tarde!',
 ]
 
-const chanceText = () => {
-  const totalTexts = randomText.length;
-  const totalCalc = 1 / totalTexts;
-  const calcPercent = totalCalc * 100;
-  const text = `A chance dessa mensagem aparecer é de ${calcPercent}%`,
-  randomText.push(text);
-}
+// const chanceText = () => {
+//   const totalTexts = randomText.length;
+//   const totalCalc = 1 / totalTexts;
+//   const calcPercent = totalCalc * 100;
+//   const text = `A chance dessa mensagem aparecer é de ${calcPercent}%`,
+//   randomText.push(text);
+// }
 
 export const randomTextSelector = () => {
   const newMin = Math.ceil(1);
   const newMax = Math.floor(randomText.length + 1);
-  chanceText();
+//   chanceText();
   const randomPosition = Math.floor(Math.random() * (newMax - newMin)) + newMin;
   return randomText[randomPosition];
 }
